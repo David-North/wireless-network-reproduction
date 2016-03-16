cd63ad2599f9dbdb666f71f933dda5987fd98f13
FinalTheory <FinalTheory@hotmail.com>
2016-03-16 15:37:14 +0800

    Add github pages

--- macdivert/__init__.py
+++
@@ -1,4 +0,0 @@
-# encoding: utf8
-from macdivert import *
-from emulator import *
-import libdivert as nids
--- network_emulator.py
+++
@@ -1,8 +0,0 @@
-import Tkinter as tk
-from macdivert.emulator import EmulatorGUI
-
-__author__ = 'huangyan13@baidu.com'
-
-
-if __name__ == '__main__':
-    EmulatorGUI(tk.Tk()).mainloop()
